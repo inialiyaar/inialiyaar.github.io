a336e6fcfed400608eeb397ef80580b41463fce7
inialiyaar <inialiyaar@gmail.com>
2025-12-21 14:13:29 +0530

    Create home page

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("home").hidden = false;
+  }, 4000);
+});
