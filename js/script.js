6317379a53f295f87839afb8cefdeb8605016ce2
inialiyaar <inialiyaar@gmail.com>
2025-12-23 15:48:46 +0530

    Design home page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+  const menuBtn = document.getElementById("menu-bar");
+  const sideMenu = document.getElementById("sideMenu");
+  const closeBtn = document.getElementById("close-side-menu");
+
   setTimeout(() => {
     document.getElementById("loader").style.display = "none";
     document.getElementById("home").hidden = false;
   }, 1500);
+
+  menuBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    sideMenu.classList.toggle("active");
+  });
+
+  closeBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    sideMenu.classList.remove("active");
+  });
+
+  sideMenu.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+
+  document.addEventListener("click", () => {
+    sideMenu.classList.remove("active");
+  });
+
 });
