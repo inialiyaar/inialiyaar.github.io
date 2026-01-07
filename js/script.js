200176c9ef5a9a8d5c98f1d476a4330d698f19f5
inialiyaar <inialiyaar@gmail.com>
2026-01-07 15:51:54 +0530

    Add mejor update

--- js/script.js
+++ js/script.js
@@ -1,37 +1,15 @@
-let menu_btn = document.getElementById("menu-bar");
-let side_menu = document.getElementById("sideMenu");
-let close_btn = document.getElementById("close-side-menu");
-let body = document.body
-let about = document.getElementById("about-text");
-
-setTimeout(() => {
-  if (!document.getElementById("loader")) return;
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("home").hidden = false;
-}, 1500);
-
-const ShowMenu = ((event) => {
-  event.stopPropagation();
-  side_menu.classList.toggle("PushMenu");
-});
-
-const CloseMenu = ((event) => {
-  event.stopPropagation();
-  side_menu.classList.remove("PushMenu");
-});
-menu_btn.addEventListener("click", ShowMenu);
-close_btn.addEventListener("click", CloseMenu);
-
-body.addEventListener("click", CloseMenu);
-
+let about = document.getElementById("about-text")
 let aboutList = ["Bot developer", "Web developer", "Python programmer",];
-
 let aboutListIndex = 0;
 let charIndex = 0;
 let text = "";
 
+setTimeout(() => {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("home").hidden = false;
+}, 1500);
+
 function typeWriter() {
-  if (!about) return;
   if (charIndex < aboutList[aboutListIndex].length) {
     text += aboutList[aboutListIndex].charAt(charIndex);
     about.innerHTML = text + "<span> |</span>";
@@ -58,45 +36,77 @@ function erase() {
 
 typeWriter();
 
-let Music = document.querySelector(".music");
 
-function addMusicCard(img, title, artist, duration, lyrics) {
-  if (!Music) return;
-  let musicCard = document.createElement("div");
-  musicCard.setAttribute("class", "hover");
-  Music.append(musicCard);
-  
-  let image = document.createElement("img");
-  image.setAttribute("src", img);
-  image.setAttribute("alt", title);
-  musicCard.append(image);
-  
-  let h2Title = document.createElement("h2");
-  musicCard.append(h2Title);
-  h2Title.innerHTML = title;
-  
-  let h3Artist = document.createElement("h3");
-  musicCard.append(h3Artist);
-  h3Artist.innerHTML = artist;
-  
-  let spanDuration = document.createElement("span");
-  musicCard.append(spanDuration);
-  spanDuration.innerHTML = duration;
-  
-  let aLyrics = document.createElement("a");
-  aLyrics.innerHTML = "lyrics";
-  aLyrics.setAttribute("href", lyrics);
-  aLyrics.setAttribute("target", "_blank");
-  musicCard.append(aLyrics);
-};
+Array.from(document.querySelector(".exploreBtn").getElementsByTagName("button")).forEach((e, idx, arr) => {
+  e.addEventListener("click", () => {
+    arr.forEach((e) => {
+      e.classList.remove("active");
+    })
+    e.classList.add("active");
+    Array.from(document.getElementsByClassName("content")).forEach(elm => {
+      if (elm.getAttribute("class").split(" ")[0] === e.getAttribute("id").slice(0, -3)) {
+        elm.style.display = "flex";
+      }
+      else {
+        elm.style.display = "none";
+      }
+    })
+  })
+})
 
-addMusicCard("../MyMusic/assets/tu-hai-kahan.jpeg", "Tu Hai Kahan", "AUR", "Duration: 4 minutes 24 seconds", "https://www.smule.com/song/uraan-tu-hai-kahan-karaoke-lyrics/19442783_19442783/arrangement")
-addMusicCard("../MyMusic/assets/kabhi-kabhi.jpeg", "Kabhi Kabhi", "AUR", "Duration: 3 minutes 36 seconds", "https://genius.com/Aur-kabhi-kabhi-lyrics")
-addMusicCard("../MyMusic/assets/shikayat.jpeg", "Shikayat", "AUR", "Duration: 4 minutes 33 seconds", "https://genius.com/Genius-romanizations-aur-shikayat-romanized-lyrics")
-addMusicCard("../MyMusic/assets/meherban.jpeg", "Meherban", "JANI", "Duration: 3 minutes 32 seconds", "https://genius.com/Jani-and-jokhay-meherban-lyrics")
-addMusicCard("../MyMusic/assets/tu-bata.jpeg", "Tu Bata", "Aftab", "Duration: 2 minutes 40 seconds", "https://www.shazam.com/song/1818727612/tu-bata")
-addMusicCard("../MyMusic/assets/you.jpeg", "You", "AUR", "Duration: 4 minutes 03 seconds", "https://genius.com/Aur-you-lyrics/")
-addMusicCard("../MyMusic/assets/you.jpeg", "Me", "AUR", "Duration: 4 minutes 03 seconds", "https://genius.com/Aur-me-lyrics")
-addMusicCard("../MyMusic/assets/you.jpeg", "Purple", "AUR", "Duration: 4 minutes 42 seconds", "https://genius.com/Aur-purple-lyrics")
-addMusicCard("../MyMusic/assets/you.jpeg", "Paint", "AUR", "Duration: 2 minutes 51 seconds", "https://genius.com/Aur-paint-lyrics")
-addMusicCard("../MyMusic/assets/you.jpeg", "Again", "AUR", "Duration: 3 minutes 22 seconds", "https://genius.com/Aur-again-lyrics")
+async function fetchFiles(folder){
+  let files = []
+  let file = await fetch(`/assets/${folder}/`)
+  let textFormat = await file.text()
+  let div = document.createElement("div")
+  div.innerHTML = textFormat
+  Array.from(div.getElementsByTagName("a")).forEach((e)=>{
+    if(e.href.includes(`/assets/${folder}/`)){
+      files.push(e.href)
+    }
+  })
+  return files
+}
+
+function appendAnime(animes){
+  animes.forEach((anime)=>{
+    let div = document.querySelector(".anime")
+    let img = anime.trim();
+    anime = anime.slice(anime.indexOf("/anime/")+7).replaceAll("%20", " ").replace("%E2%80%A2", ".").replaceAll("%2C", ",").split("_")
+    anime[2] = anime[2].replace(".jpg", "");
+    anime[1].length>170?anime[1]=anime[1].slice(0, 170) + "...":anime[1]=anime[1]
+    div.innerHTML += `<div class="animeCard flex align-center hover">
+          <img src="${img}" alt="${anime[0]}">
+          <div class="flex flex-column">
+            <h4>${anime[0]}</h4>
+            <p>${anime[1]}</p>
+            <span>⭐ ${anime[2]}/10</span>
+          </div>
+        </div>`
+  })
+}
+
+function appendMusic(musics){
+  musics.forEach((music)=>{
+    let img = music;
+    music = music.slice(music.indexOf("/music")+7).replaceAll("%20", " ").split("_")
+    music[2] = music[2].replace(".jpeg", "").split("-")
+    document.querySelector(".music").innerHTML += `<div class="musicCard flex align-center hover">
+          <img src="${img}" alt="${music[0]}">
+          <div class="flex flex-column">
+            <h4>${music[0]}</h4>
+            <p>${music[1]}</p> 
+            <p>Duration: ${music[2][0]} minutes ${music[2][1]} seconds</p>
+          </div>
+        </div>`
+  })
+}
+
+async function main(){
+  let animes = await fetchFiles("anime")
+  appendAnime(animes)
+  let musics = await fetchFiles("music")
+  appendMusic(musics)
+}
+
+main()
